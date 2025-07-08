0b524376a392d72c43a6e8c8158b40061dcc4b4c
IndhuMuruganantham <157617725+IndhuMuruganantham@users.noreply.github.com>
2025-07-08 20:40:57 +0530

    Create Input & Output - ASCII Values - I.java

---
+++ Input & Output - ASCII Values - I.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        char c=s.next().charAt(0);
+        int t=(int)c;
+        System.out.println(t);
+    }
+}
