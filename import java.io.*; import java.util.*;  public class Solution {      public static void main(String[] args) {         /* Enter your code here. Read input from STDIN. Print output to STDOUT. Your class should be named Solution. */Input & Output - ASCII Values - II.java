9f8972938c3d62d054c1e984d99c7760962c4edf
IndhuMuruganantham <157617725+IndhuMuruganantham@users.noreply.github.com>
2025-07-08 20:42:31 +0530

    Create Input & Output - ASCII Values - II.java

---
+++ import java.io.*; import java.util.*;  public class Solution {      public static void main(String[] args) {         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */Input & Output - ASCII Values - II.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        char m=(char)t;
+        System.out.println(m);
+    }
+}
